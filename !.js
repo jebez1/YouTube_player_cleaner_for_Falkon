c7b6e40ffedd055a8b4cef0a073f4a4c020adca0
Jérémy Bézairie <Jeremy_Bezairie@outlook.com>
2025-01-27 09:47:08 +0000

    Update !.js

--- !.js
+++ !.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @include https://www.youtube.com/watch?v=*
 // ==/UserScript==
-document.getElementsByClassName('ytp-gradient-top')[0].remove()
-document.getElementsByClassName('ytp-chrome-top')[0].remove()
-document.getElementsByClassName('ytp-gradient-bottom')[0].remove()
-document.getElementsByTagName('video')[0].addEventListener('pause',()=>document.getElementsByClassName('ytp-bezel-text-hide')[0].remove(),{once:true})
-b=document.getElementsByTagName('video')[0]
-c=document.getElementsByClassName('ytp-chrome-bottom')[0]
+document.getElementsByClassName("ytp-gradient-top")[0].remove()
+document.getElementsByClassName("ytp-chrome-top")[0].remove()
+document.getElementsByClassName("ytp-gradient-bottom")[0].remove()
+document.getElementsByTagName("video")[0].addEventListener("pause",()=>document.getElementsByClassName("ytp-bezel-text-hide")[0].remove(),{once:true})
+b=document.getElementsByTagName("video")[0]
+c=document.getElementsByClassName("ytp-chrome-bottom")[0]
 d=document.body.style
 let a
-b.onmousemove=()=>{d.cursor='initial'
+b.onmousemove=()=>{d.cursor="initial"
 clearTimeout(a)
-a=setTimeout(()=>d.cursor='none',500)}
-b.onmouseleave=()=>setTimeout(()=>d.cursor='initial',500)
+a=setTimeout(()=>d.cursor="none",500)}
+b.onmouseleave=()=>setTimeout(()=>d.cursor="initial",500)
 c.onmouseenter=()=>c.style.opacity=1
 c.onmouseleave=()=>c.style.opacity=0
