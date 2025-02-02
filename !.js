c4b98b8d6dc10bde58056bd59e74d4992cc15fcf
Jérémy Bézairie <Jeremy_Bezairie@outlook.com>
2025-02-02 07:47:38 +0000

    Update !.js

--- !.js
+++ !.js
@@ -4,15 +4,14 @@
 document.getElementsByClassName("ytp-gradient-top")[0].remove()
 document.getElementsByClassName("ytp-chrome-top")[0].remove()
 document.getElementsByClassName("ytp-gradient-bottom")[0].remove()
+document.getElementsByTagName("video")[0].addEventListener("pause",()=>document.getElementsByClassName("ytp-bezel-text-hide")[0].remove(),{once:true})
 a=document.getElementsByTagName("video")[0]
-a.addEventListener("pause",()=>document.getElementsByClassName("ytp-bezel-text-hide")[0].remove(),{once:true})
-b=document.body.style
-c=document.getElementsByClassName("ytp-chrome-bottom")[0]
-d=c.style
+b=document.getElementsByClassName("ytp-chrome-bottom")[0]
+c=document.body.style
 let $
-a.onmousemove=()=>{b.cursor=d.visibility=""
+a.onmousemove=()=>{c.cursor=""
 clearTimeout($)
-$=setTimeout(()=>{b.cursor="none"
-d.visibility="hidden"},500)}
-a.onmouseleave=()=>setTimeout(()=>b.cursor="",500)
-c.onmouseenter=()=>setTimeout(()=>d.visibility="",500)
+$=setTimeout(()=>c.cursor="none",500)}
+a.onmouseleave=()=>setTimeout(()=>c.cursor="",500)
+b.onmouseenter=()=>b.style.opacity=1
+b.onmouseleave=()=>b.style.opacity=0
