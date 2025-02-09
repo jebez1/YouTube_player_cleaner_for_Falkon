deddee762c5449ebf036e7c332e9b08f30e09084
Jérémy Bézairie <Jeremy_Bezairie@outlook.com>
2025-02-09 21:40:00 +0000

    Update !.js

--- !.js
+++ !.js
@@ -7,12 +7,12 @@ document.getElementsByClassName("ytp-gradient-bottom")[0].remove()
 a=document.getElementsByTagName("video")[0]
 a.addEventListener("pause",()=>document.getElementsByClassName("ytp-bezel-text-hide")[0].remove(),{once:true})
 b=document.body.style
-c=document.getElementsByClassName("ytp-chrome-bottom")[0]
-d=c.style
 let $
 a.onmousemove=()=>{b.cursor=""
 clearTimeout($)
 $=setTimeout(()=>b.cursor="none",500)}
 a.onmouseleave=()=>setTimeout(()=>b.cursor="",500)
+c=document.getElementsByClassName("ytp-chrome-bottom")[0]
+d=c.style
 c.onmouseenter=()=>d.opacity=1
 c.onmouseleave=()=>d.opacity=0
